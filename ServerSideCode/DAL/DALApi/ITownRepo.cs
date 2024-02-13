bf8a886134370ad2c0bb396b51ce01b65cc68401
הורוביץ לאה < @C26>
2024-02-13 15:55:04 +0200

    add IRepos

--- ServerSideCode/DAL/DALApi/ITownRepo.cs
+++ ServerSideCode/DAL/DALApi/ITownRepo.cs
@@ -1,12 +1,13 @@
-﻿using System;
+﻿using DAL.Models;
+using DBAcsess;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DAL.DALApi
+namespace DAL.DALApi;
+
+public interface ITownRepo:IRepository<Town>
 {
-    internal interface ITownRepo
-    {
-    }
 }
