84cd35907380bd4c9849a4457f1cf4d0e8ef90d2
הורוביץ לאה < @C26>
2024-05-07 12:49:31 +0300

    add client

---
+++ DB/dbo.Regions.data.sql
@@ -0,0 +1,9 @@
+﻿SET IDENTITY_INSERT [dbo].[Regions] ON
+INSERT INTO [dbo].[Regions] ([Code], [RegionName]) VALUES (100, N'Golan')
+INSERT INTO [dbo].[Regions] ([Code], [RegionName]) VALUES (101, N'Upper Galilee')
+INSERT INTO [dbo].[Regions] ([Code], [RegionName]) VALUES (102, N'Lower Galilee')
+INSERT INTO [dbo].[Regions] ([Code], [RegionName]) VALUES (103, N'The Coastal Plain')
+INSERT INTO [dbo].[Regions] ([Code], [RegionName]) VALUES (104, N'Jerusalem')
+INSERT INTO [dbo].[Regions] ([Code], [RegionName]) VALUES (105, N'Negev')
+INSERT INTO [dbo].[Regions] ([Code], [RegionName]) VALUES (106, N'North')
+SET IDENTITY_INSERT [dbo].[Regions] OFF
---
+++ ServerSideCode/DAL/DALApi/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBAcsess;
+
+public interface IRepository<T>
+{
+    Task<List<T>> GetAllAsync(BaseQueryParams queryParams);
+    Task<T> GetSingleAsync(int id);
+    Task<T> AddAsync(T entity);
+    Task<T> UpdateAsync(T entity);
+    Task<T> DeleteAsync(int id);
+}
